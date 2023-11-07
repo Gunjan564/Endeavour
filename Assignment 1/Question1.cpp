3bfbea3bce77cbd1aafe1c1ca7260f112e8fbb3b
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-08 00:50:05 +0530

    Create Question1.cpp

---
+++ Assignment 1/Question1.cpp
@@ -0,0 +1,24 @@
+/*Check whether the number is positve or negative 
+integer using if else*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"GUNJAN"<<endl;
+    cout<<"Enter the value of integer a: ";
+    cin>>a;
+    if (a>0)
+    {
+            cout<<"a is a positive integer";
+    }
+    if(a<0)
+    {    
+        cout<<"a is a negative integer";   
+    }
+    if(a==0)
+    {
+        cout<<"a is zero integer";
+    }
+    
+}
