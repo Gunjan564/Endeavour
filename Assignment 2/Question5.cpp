ad92f91aab4c51b7be3724a9d57e294afe668880
Gunjan564 <145559194+Gunjan564@users.noreply.github.com>
2023-11-30 23:54:18 +0530

    Update Assignment 2

---
+++ Assignment 2/Question5.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int sum()
+{
+
+}
+int main()
+{
+    int i,r,c;
+    cout<<"GUNJAN"<<endl;
+    cout<<"Enter the value of r: ";
+    cin>>r;
+    for(i=1;i<=r;i++)
+    {
+        for(c=1;c<i+1;c++)
+        {
+            cout<<c<<" ";
+        } 
+        cout<<endl;  
+    }
+
+
+}
