54f2d0dd674c18cec0373f284bb53c0753100d3e
Gunjan <gunjan231103@gmail.comm>
2023-12-06 23:54:40 +0530

    stereoheart

---
+++ Assignment 3/firstprogram.c
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n,fact;
+    cout<<"Enter the value of n: ";
+    cin>>n;
+    for(i=1,fact=1;i<=n;i++)
+    {
+        fact=i*fact;
+        if(i==n)
+        {
+            cout<<i;
+            break;
+        }
+        cout<<i<<" * ";
+    }
+    cout<<" = "<<fact;
+    return 0;
+}
